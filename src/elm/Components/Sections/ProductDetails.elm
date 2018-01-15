d992c6008bbeba9010085cfeddc2352fd91a91cf
Asaf Cohen <safhac@gmail.com>
2018-01-15 13:19:05 +0200

    product details page

--- src/elm/Components/Sections/ProductDetails.elm
+++ src/elm/Components/Sections/ProductDetails.elm
@@ -1,13 +1,18 @@
 module Components.Sections.ProductDetails exposing (..)
 
-import Html.Styled exposing (Html, div, text, main_, img, h2, h3, a)
+import Html.Styled exposing (Html, div, text, main_, img, h2, h3, a, button)
 import Html.Styled.Attributes exposing (css, href, src, id)
 import Html.Styled.Events exposing (onClick)
 import Routing.Routes exposing (..)
 import Types exposing (..)
 import Styles.Styles exposing (..)
+import Helpers.Common exposing (..)
 
 
 renderProduct : Product -> Html Msg
 renderProduct product =
-    div [] []
+    div [ id "product" ]
+        [ h2 [] [ text product.name ]
+        , img [ src (productsImageFolder ++ product.linkUrl) ] []
+        , text product.name
+        ]
