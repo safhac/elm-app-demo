683874cfb569d482a2f56825db5dc7b1f16a98d3
Asaf Cohen <safhac@gmail.com>
2018-01-15 11:51:51 +0200

    added pages

---
+++ src/elm/Components/Pages/Register.elm
@@ -0,0 +1,10 @@
+module Components.Sections.Pages.Register exposing (..)
+
+import Html.Styled.Events exposing (onClick)
+import Routing.Routes exposing (..)
+import Types exposing (..)
+import Styles.Styles exposing (..)
+
+
+view =
+    ""
